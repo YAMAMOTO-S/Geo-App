167de0a258c9532e3c158e6a1920a55f0ffb9bf4
Shunya <yamasyunizig1216@gmail.com>
2019-10-16 21:27:21 +0900

    first & Google Map API を表示させた

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <nav class="orange lighten-3">
+      <div class="container">
+        <a href="" class="brand-logo left">Geo-App</a>
+        <ul class="right">
+          <li><a href="">Signup</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data(){
+    return{
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/firebase/init.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAsfT9RSj4CpaDlRlBKX1mLhrARacQkeYk",
+  authDomain: "geo-app-60123.firebaseapp.com",
+  databaseURL: "https://geo-app-60123.firebaseio.com",
+  projectId: "geo-app-60123",
+  storageBucket: "geo-app-60123.appspot.com",
+  messagingSenderId: "533148173268",
+  appId: "1:533148173268:web:e7f237b6737f21dfb98c41",
+  measurementId: "G-6Z6TDNNBJT"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore()
