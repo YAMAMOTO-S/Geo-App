f9d3c1ae06f5905082fa63b74fe426abba6765be
Shunya <yamasyunizig1216@gmail.com>
2019-10-20 17:48:37 +0900

    User record を登録時に作成できるようにした。

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -55,8 +55,16 @@ export default {
           } else {
           // this alias does not yet exists in the db
             firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            .then(cred => {
+              ref.set({
+                alias: this.alias,
+                geolocation: null,
+                user_id: cred.user.uid
+              })
+            }).then(() => {
+              this.$router.push({ name: 'GMap' })
+            })
             .catch(err => {
-              console.log(err.message)
               this.feedback = err.message
             })
           }
