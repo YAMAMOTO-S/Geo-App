7a85374fc2ffe6477282c9f77f53c70a2ce84057
Shunya <yamasyunizig1216@gmail.com>
2019-10-21 11:38:14 +0900

    Login ページの作成

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,51 @@
+<template>
+   <div class="login container">
+      <form @submit.prevent="login" class="card-panel">
+         <h2 class="center">login</h2>
+         <div class="field">
+            <label for="email">Email:</label>
+            <input type="email" name="email" v-model="email">
+         </div>
+         <div class="field">
+            <label for="password">Password:</label>
+            <input type="password" name="password" v-model="password">
+         </div>
+         <p v-if='feedback' class="red-text center">{{ feedback }}</p>
+         <div class="field center">
+            <button class="btn deep-orange lighten-3">Login</button>
+         </div>
+      </form>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Login',
+   data(){
+      return{
+         email: null,
+         password: null,
+         feedback: null,
+      }
+   },
+   methods: {
+      login(){
+         console.log(this.email, this.password)
+      }
+   }
+}
+</script>
+
+<style>
+.login h2{
+   color: #F99273;
+   margin-bottom: 50px;
+}
+.login{
+   margin-top: 60px;
+   max-width: 800px;
+}
+.login .field{
+   margin-bottom: 20px;
+}
+</style>
