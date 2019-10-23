bd9210897807ffdc9b03dc8f7516f72a82e48974
Shunya <yamasyunizig1216@gmail.com>
2019-10-24 00:23:42 +0900

    Gmap に行くと現在地に行くように

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from 'firebase'
+import db from '@/firebase/init'
 
 export default {
   name: 'GMap',
@@ -27,8 +28,20 @@ export default {
     }
   },
   mounted(){
-    this.renderMap()
-    console.log(firebase.auth().currentUser)
+    //get current location
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.lat = pos.coords.latitude
+        this.lng = pos.coords.longitude
+        this.renderMap()
+      }, (err) => {
+        console.log(err)
+        this.renderMap()
+      }, { maximumAge: 60000, timeout: 3000 }) // cached location
+    } else {
+      // position centre by default values
+      this.renderMap()
+    }   
   }
 }
 </script>
