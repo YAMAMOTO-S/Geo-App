0eef6189bb6ce9e13ca7d6752f2d895346670316
Shunya <yamasyunizig1216@gmail.com>
2019-10-24 23:14:18 +0900

    DBにコメントを送れるように、 誰が誰に送ったかも分かるようにした

--- src/components/profile/ViewPlofile.vue
+++ src/components/profile/ViewPlofile.vue
@@ -2,30 +2,77 @@
    <div class="profile container">
       <div class="card" v-if="profile">
          <h2 class="center">{{ profile.alias}}</h2>
+         <ul class="comments collection">
+            <li>Comment</li>
+         </ul>
+         <form @submit.prevent="addComment">
+            <div class="field">
+               <label for="comment">Add a comment</label>
+               <input type="text" name="comment" v-model="newComment">
+
+               <p v-if="feedback" class="red-text center">{{feedback}}</p>
+            </div>
+         </form>
       </div>
    </div>
 </template>
 
 <script>
 import db from '@/firebase/init'
-
+import firebase from 'firebase'
 export default {
-   name: 'ViewProfile',
-   data(){
-      return{
-         profile: null,
-      }
-   },
-   created(){
-      let ref = db.collection('users')
-      ref.doc(this.$route.params.id).get()
-      .then(user => {
-         this.profile = user.data()
+  name: 'ViewProfile',
+  data(){
+    return{
+      profile: null,
+      newComment: null,
+      feedback: null
+    }
+  },
+  created(){
+    let ref = db.collection('users')
+
+    // current user
+    ref.where('user_id', '==', firebase.auth().currentUser.uid).get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        this.user = doc.data()
+        this.user.id = doc.id
       })
-   }
+    })
+
+    // profile data
+    ref.doc(this.$route.params.id).get()
+    .then(user => {
+      this.profile = user.data()
+    })
+  },
+
+  
+  methods: {
+    addComment(){
+      if(this.newComment){
+        this.feedback = null
+        db.collection('comments').add({
+          to: this.$route.params.id,
+          from: this.user.id,
+          content: this.newComment,
+          time: Date.now()
+        }).then(doc => {
+          this.newComment = null //ここでリセットして空白にする。
+        })
+      } else {
+        this.feedback = 'You must enter something'
+      }
+    }
+  }
 }
 </script>
 
 <style>
-
+.profile h2{
+   color: #F99273;
+   margin-bottom: 50px;
+   font-size: 50px;
+}
 </style>
