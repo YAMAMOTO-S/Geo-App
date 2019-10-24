9fc2ec908484d975b6811629b65cb9457845b073
Shunya <yamasyunizig1216@gmail.com>
2019-10-24 13:46:12 +0900

    router guards 実装

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,20 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import firebase from 'firebase'
 
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -26,3 +30,23 @@ export default new Router({
     }
   ]
 })
+
+// router guards
+router.beforeEach((to, from, next) => {
+  // check to see if route has auth guard
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check auth state of user
+    let user = firebase.auth().currentUser
+    if (user) {
+      next()
+    } else {
+      next({
+        name: 'Login'
+      })
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
